928abd27eadd5d3fe1c5342efd400d7b523ea0de
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-20 17:02:22 -0300

    feat: project styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Login from './pages/Login/Login.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global.js";
+import theme from "./styles/theme.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Login />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {/* <AuthProvider> */}
+      {/* <Routes /> */}
+      {/* </AuthProvider> */}
+    </ThemeProvider>
+  </React.StrictMode>
+);
