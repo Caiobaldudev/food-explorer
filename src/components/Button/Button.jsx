693532697642438aebf0fdbb23f488c8f8a77c6d
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-20 20:11:16 -0300

    feat: button handleClick

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { ButtonC } from "./style";
 
-export const Button = ({ title, height, width }) => {
-
+export const Button = ({ title, height, width, ...props }) => {
+  function handleClick(event) {
+    event.preventDefault()
+    if (props.onClick) {
+      props.onClick();
+    }
+  }
   return (
-    <ButtonC type="button" height={height} width={width}>
+    <ButtonC type="submit" height={height} width={width} onClick={handleClick}>
       {title}
     </ButtonC>
   );
