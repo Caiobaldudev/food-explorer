2d2d0cfe2c0e2bb72dc48681c2ac970a5c069ee5
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-21 15:20:31 -0300

    feat: distributing routes according to profile

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,12 +4,24 @@ import AppRouter from "./app.routes";
 import { useAuth } from "../hooks/auth";
 import AuthRoutes from "./auth.routes";
 import Header from "../components/Header/Header";
+import { AdminRoutes } from "./admin.routes";
+import { USER_ROLE } from "../utils/roles";
 
 const Routes = () => {
   const { user } = useAuth();
+  function AcessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <AppRouter />;
+      default:
+        return <AppRouter />;
+    }
+  }
   return (
     <BrowserRouter>
-      {user ? <AppRouter /> && <Header /> : <AuthRoutes />}
+      {user ? <AcessRoute /> && <Header /> : <AuthRoutes />}
     </BrowserRouter>
   );
 };
